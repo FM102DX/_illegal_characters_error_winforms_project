9ed72236f39140cb3d77c7279d83e2f5ac063325
FM102DX <78497211+FM102DX@users.noreply.github.com>
2023-07-21 13:05:15 +0300

    First commit

---
+++ RifdcOrmWithExample/Form1.Designer.cs
@@ -0,0 +1,23 @@
+﻿namespace RifdcOrmWithExample
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "Form1";
+        }
+    }
+}
